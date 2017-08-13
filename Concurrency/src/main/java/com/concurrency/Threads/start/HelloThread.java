901925668b9778ea296d7273ab3bb12f7e805563
ashik.khan <ashik.khan@N1020.spftl.com>
2017-08-13 09:37:19 +0600

    Added 2 PL/SQL packages and a java file..

--- Concurrency/src/main/java/com/concurrency/Threads/start/HelloThread.java
+++ Concurrency/src/main/java/com/concurrency/Threads/start/HelloThread.java
@@ -6,7 +6,7 @@ public class HelloThread extends Thread {
 	public void run() {
 		System.out.println("Hello from a thread!");
 	}
-	
+
 	public static void main(String[] args) {
 		new HelloThread().start();
 	}
